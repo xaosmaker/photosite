c543d46239e58f182f85e3a9c7f1c0ef0eaa70e4
Drosos Drosinakis <drososD@proton.me>
2025-09-30 23:13:46 +0300

    chore: fix the eslint errors to ignore the ingores

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -14,14 +14,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [
-      "node_modules/**",
-
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ["node_modules", ".next", "out", "build", "next-env.d.ts"],
+  },
+  {
     plugins: { boundaries: boundaries },
     ...conf,
   },
