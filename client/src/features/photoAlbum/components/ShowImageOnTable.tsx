9f67768002bd945eda0d6f90f33fe64f07b4bdff
Drosos Drosinakis <drososD@proton.me>
2025-10-04 19:53:20 +0300

    fix: showimageonTable fix how the image is shown

--- client/src/features/photoAlbum/components/ShowImageOnTable.tsx
+++ client/src/features/photoAlbum/components/ShowImageOnTable.tsx
@@ -1,14 +1,9 @@
-import { Button } from "@/components/ui/button";
 import {
   Dialog,
-  DialogClose,
-  DialogOverlay,
-  DialogPortal,
   DialogContent,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { X } from "lucide-react";
 import Image from "next/image";
 export default function ShowImageOnTable({
   src,
@@ -22,28 +17,19 @@ export default function ShowImageOnTable({
       <DialogTrigger>
         <Image width={50} height={50} src={src} alt={alt} />
       </DialogTrigger>
-      <DialogPortal>
-        <DialogOverlay>
-          <DialogContent className="flex h-screen w-screen flex-col items-center justify-center">
-            <DialogTitle className="hidden">
-              A cover image of the photo album
-            </DialogTitle>
-            <div className="relative h-3/4 w-3/4">
-              <DialogClose asChild className="absolute top-4 right-4 z-10">
-                <Button>
-                  <X />
-                </Button>
-              </DialogClose>
-              <Image
-                fill
-                src={src}
-                className="h-full w-full object-cover"
-                alt={alt}
-              />
-            </div>
-          </DialogContent>
-        </DialogOverlay>
-      </DialogPortal>
+      <DialogContent className="h-full w-full sm:max-w-full">
+        <DialogTitle className="hidden">
+          A cover image of the photo album
+        </DialogTitle>
+        <div className="relative">
+          <Image
+            fill
+            src={src}
+            className="h-full w-full object-contain"
+            alt={alt}
+          />
+        </div>
+      </DialogContent>
     </Dialog>
   );
 }
